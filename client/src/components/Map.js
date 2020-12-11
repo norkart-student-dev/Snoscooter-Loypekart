7b951d6a9c790e7f2b0adc7fdf720cfa94b0207d
Bjørn <bm_93@hotmail.com>
2020-12-11 13:45:54 +0100

    Map now displays all points of interest from the database on page load

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { MapContainer, TileLayer} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
+import { parkingIcon } from './Icons'
 import 'leaflet/dist/leaflet.css';
 
 export default function Mapview(props){
+    
+   
     return(
         <MapContainer className='Map' center={[60.39, 5.32]} zoom={13}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            {props.pois !== undefined && props.pois.map((item, index) => {
+                return(
+                    <Marker position={item.location.coordinates} key={item._id} icon={parkingIcon} >
+                </Marker>
+                );
+            })}
         </MapContainer>
     )
 }
