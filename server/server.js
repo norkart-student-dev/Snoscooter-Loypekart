1749cde4311f8fa23e6facd375edd64f5fe9822f
xorfindude <fuglestad.simen@gmail.com>
2020-12-13 13:05:47 +0100

    added express client production serving

--- server/server.js
+++ server/server.js
@@ -18,3 +18,11 @@ app.use('/poi', poiRouter)
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
