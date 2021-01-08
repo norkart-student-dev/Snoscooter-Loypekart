2f274adeade63f1464e4499822abb9bd8d92a976
Bjørn <bm_93@hotmail.com>
2021-01-08 14:06:49 +0100

    added prompt befor moving poi

--- client/src/components/PoiMarker.js
+++ client/src/components/PoiMarker.js
@@ -66,7 +66,7 @@ import Icon from './Icon';
                     </p> : null}
 
                     {user.loggedIn && <button onClick={() => {editPoi(item._id); closePopup();}}>Endre</button>}
-                    {user.loggedIn && <button onClick={() => {setMoving(item._id); closePopup();}}>Flytt</button>}
+                    {user.loggedIn && <button onClick={() => { if (window.confirm('Hvis du ønsker og flytte punktet, så klikk ok, deretter klikk i kartet der det skal flyttes til. Hvis ikke klikk avbryt')) setMoving(item._id); closePopup();}}>Flytt</button>}
                     {user.loggedIn && <button onClick={() => { if (window.confirm('Er du sikker på at du vil slette dette punktet?')) deletePoi(item._id); closePopup();}}>Slett</button>}
 
                 </Popup>
